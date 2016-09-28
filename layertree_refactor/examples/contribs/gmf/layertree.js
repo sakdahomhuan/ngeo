e68cd41e3d52ac82cbfedb2b9f2e9c1ece701880
Travis <travis@travis-ci.org>
2016-09-28 09:42:53 +0000

    Update GitHub pages

--- layertree_refactor/examples/contribs/gmf/layertree.js
+++ layertree_refactor/examples/contribs/gmf/layertree.js
@@ -174,8 +174,6 @@ app.MainController = function(gmfThemes, gmfTreeManager, ngeoLocation) {
         }
       }.bind(this));
 
-      //set arbitrarily a default theme
-      this.gmfTreeManager.addTheme(themes[0]);
       this.treeSource = this.gmfTreeManager.tree;
     }
   }.bind(this));
